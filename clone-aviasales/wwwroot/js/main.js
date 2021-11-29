7dbe004e3e376c5622c092dd278e680d19d8af78
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2021-11-29 08:04:49 +0300

    refactor: make code more readable and flexible

--- clone-aviasales/wwwroot/js/main.js
+++ clone-aviasales/wwwroot/js/main.js
@@ -1,96 +1,125 @@
 ﻿$(function () {
     let originIata = $('input[name = origin_iata]'),
         destinationIata = $('input[name = destination_iata]'),
-        departDate = $('input[name = depart_date]'),
-        returnDate = $('input[name = return_date]'),
-        originName = $('.twidget-origin .twidget-pseudo-name'),
-        destinationName = $('.twidget-destination .twidget-pseudo-name');
+        departDate = $('input[name = depart_date]');
+    const locale = 'ru-RU';
+    const purchaseUrl = 'https://aviasales.ru';
+    const imagesUrl = 'https://pics.avs.io/al_square/36/36/';
+    const minutesPerDay = 60 * 24;
+    const minutesPerHour = 60;
+    const millisecondsPerMinute = 60 * 1000;
+    const fetchTimeFormatOptions = (timezone) => ({ hour: '2-digit', minute: '2-digit', timeZone: timezone });
+    const fetchDateFormatOptions = (timezone) => ({ weekday: 'short', day: 'numeric', month: 'short', year: 'numeric', timeZone: timezone });
 
     $('#search-form').submit(function (event) {
         event.preventDefault();
 
         if (isFormValid()) {
-            let originIataValue = originIata.val(),
-                destinationIataValue = destinationIata.val(),
-                departDateValue = departDate.val(),
-                returnDateValue = returnDate.val();
+            let formData = $(this).serialize();
 
-            console.log(departDateValue);
-
-            let test = $(this).serialize();
-
-            console.log(test);
-
-            $.ajax({
-                type: 'GET',
-                url: 'api/ticket',
-                dataType: 'json',
-                data: test,
-                success: function (data) {
-                    loadTickets(data);
-                },
-                error: function (error) {
-                    $('.not-found').css("display", "block");
-                }
-            });
+            fetchTickets(formData);
         }
     });
 
     function isFormValid() {
-        return originIata.val() && destinationIata.val() && departDate.val() /*&& returnDate.val()*/;
+        return originIata.val() && destinationIata.val() && departDate.val();
     }
 
-    function loadTickets(data) {
-        let tickets = data.data;
-        let cities = data.cities;
-        let airlines = data.airlines;
+    function isResponseValid(response) {
+        return response.success === true &&
+            response.data != null &&
+            response.data.length != 0 &&
+            response.airlines != null &&
+            response.airlines.length != 0 &&
+            response.cities != null &&
+            response.cities.length != 0;
+    }
 
-        for (obj of tickets) {
-            var durat = Number(obj.duration);
-            var d = Math.floor(durat / (60 * 24));
-            var h = Math.floor(durat % (60 * 24) / 60);
-            var m = Math.floor(durat % 60)
+    function fetchTickets(formData) {
+        $.ajax({
+            type: 'GET',
+            url: 'api/ticket',
+            dataType: 'json',
+            data: formData,
+            success: function (data) {
+                handleResponse(data);
+            },
+            error: function (error) {
+                hideTickets();
+                clearTickets();
+                showNotFoundError();
+            }
+        });
+    }
 
-            let origin = cities[obj.origin].name;
-            let destination = cities[obj.destination].name;
-            let price = obj.price;
-            let durationHours = obj.duration / 60;
-            let durationsMinutes = obj.duration % 60;
-            let link = obj.link;
-            let departDate = new Date(Date.parse(obj.departure_at));
-            let departDateFinish = new Date(departDate.getTime() + durat * 60 * 1000);
-            let returnDate = new Date(Date.parse(obj.return_at));
+    function handleResponse(response) {
+        clearTickets();
 
-            let html = '<div class="card border-light rounded-15 ms-4 mb-4">' +
-                '<div class="card-body d-flex p-0">' +
-                '<div class="flight-price border-end p-3 d-flex align-items-center">' +
-                '<a class="btn btn-primary py-1 px-5" href="https://aviasales.ru' + link + '">Купить за <p>' + price + ' BYN</p></a>' +
-                '</div>' +
-                '<div class="flight-content ms-3 p-3">' +
-                '<div class="flight-content__airlines">' +
-                '<img src="https://pics.avs.io/al_square/36/36/' + obj.airline + '@2x.png" data-bs-toggle="tooltip" data-bs-placement="top" title="' + airlines[obj.airline].name + '" class="custom-img me-2 rounded-pill">' +
-                '</div>' +
-                '<div class="flight-content__description d-flex mt-3 align-items-center">' +
-                '<div class="flight-content__origin lh-base">' +
-                '<p class="fs-3">' + new Intl.DateTimeFormat('ru-RU', { hour: '2-digit', minute: '2-digit', timeZone: cities[obj.origin].timezone }).format(departDate) + '</p>' +
-                '<p class="fs-6 text-secondary">' + origin + '</p>' +
-                '<p class="fs-6 text-secondary">' + new Intl.DateTimeFormat('ru-RU', { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric', timeZone: cities[obj.origin].timezone }).format(departDate) + '</p>' +
-                '</div>' +
-                '<div class="flight-content__duration mx-5 text-center">' +
-                '<span>В пути: ' + (d ? d + 'д ' : "") + (h ? h + 'ч ' : "") + (m ? m + 'м' : "") + '</span>' +
-                '<p>(пересадок: ' + obj.transfers + ')</p>' +
-                '</div>' +
-                '<div class="flight-content__destination text-end lh-base">' +
-                '<p class="fs-3">' + new Intl.DateTimeFormat('ru-RU', { hour: '2-digit', minute: '2-digit', timeZone: cities[obj.destination].timezone }).format(departDateFinish) + '</p>' +
-                '<p class="fs-6 text-secondary">' + destination + '</p>' +
-                '<p class="fs-6 text-secondary">' + new Intl.DateTimeFormat('ru-RU', { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric', timeZone: cities[obj.destination].timezone }).format(departDateFinish) + '</p>' +
-                '</div>' +
-                '</div>' +
-                '<div class="flight-content__description d-flex mt-3 align-items-center border-top pt-3">' +
+        if (isResponseValid(response)) {
+            hideNotFoundError();
+            showTickets();
+            loadTickets(response);
+            return;
+        }
+
+        hideTickets();
+        showNotFoundError();
+    }
+
+    function hideTickets() {
+        $('.flight').css('display', 'none');
+    }
+
+    function showTickets() {
+        $('.flight').css('display', 'block');
+    }
+
+    function clearTickets() {
+        $('.flight').empty();
+    }
+
+    function showNotFoundError() {
+        $('.not-found').css('display', 'block');
+    }
+
+    function hideNotFoundError() {
+        $('.not-found').css('display', 'none');
+    }
+
+    function convertMinutesToDuration(flightDuration) {
+        let days = Math.floor(flightDuration / (minutesPerDay));
+        let hours = Math.floor(flightDuration % (minutesPerDay) / minutesPerHour);
+        let minutes = Math.floor(flightDuration % minutesPerHour);
+
+        return [days, hours, minutes];
+    }
+
+    function loadTickets(response) {
+        let tickets = response.data;
+        let cities = response.cities;
+        let airlines = response.airlines;
+
+        for (ticket of tickets) {
+            let flightDurationInMinutes = Number(ticket.duration);
+            let flightDuration = convertMinutesToDuration(flightDurationInMinutes);
+
+            let days = flightDuration[0];
+            let hours = flightDuration[1];
+            let minutes = flightDuration[2];
+
+            let origin = cities[ticket.origin].name;
+            let destination = cities[ticket.destination].name;
+            let price = ticket.price;
+            let link = ticket.link;
+            let departDate = new Date(Date.parse(ticket.departure_at));
+            let departDateFinish = new Date(departDate.getTime() + flightDurationInMinutes * millisecondsPerMinute);
+            let returnDate = ticket.return_at ? new Date(Date.parse(ticket.return_at)) : ticket.return_at;
+
+            let returnContainer = '<div class="flight-content__description d-flex mt-3 align-items-center border-top pt-3">' +
                 '<div class="flight-content__origin lh-base flex-basis-100">' +
-                '<p class="fs-3">' + new Intl.DateTimeFormat('ru-RU', { hour: '2-digit', minute: '2-digit', timeZone: cities[obj.destination].timezone }).format(returnDate) + '</p>' +
+                '<p class="fs-3">' + new Intl.DateTimeFormat(locale, fetchTimeFormatOptions(cities[ticket.destination].timezone)).format(returnDate) + '</p>' +
                 '<p class="fs-6 text-secondary">' + destination + '</p>' +
-                '<p class="fs-6 text-secondary">' + new Intl.DateTimeFormat('ru-RU', { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric', timeZone: cities[obj.destination].timezone }).format(returnDate) + '</p>' +
+                '<p class="fs-6 text-secondary">' + new Intl.DateTimeFormat(locale, fetchDateFormatOptions(cities[ticket.destination].timezone)).format(returnDate) + '</p>' +
                 '</div>' +
                 '<div class="flight-content__duration mx-5 text-center flex-basis-100">' +
                 '<img src="./images/help_black_48dp.svg" data-bs-toggle="tooltip" data-bs-placement="top" title="При покупке указано время в пути" class="custom-img me-2 rounded-pill">' +
@@ -100,7 +129,34 @@
                 '<p class="fs-6 text-secondary">' + origin + '</p>' +
                 '<img src="./images/help_black_48dp.svg" data-bs-toggle="tooltip" data-bs-placement="top" title="При покупке указана дата прибытия" class="custom-img me-2 rounded-pill">' +
                 '</div>' +
+                '</div>';
+
+            let html = '<div class="card border-light rounded-15 ms-4 mb-4">' +
+                '<div class="card-body d-flex p-0">' +
+                '<div class="flight-price border-end p-3 d-flex align-items-center">' +
+                '<a class="btn btn-primary py-1 px-5" href="' + purchaseUrl + link + '">Купить за <p>' + price + ' BYN</p></a>' +
                 '</div>' +
+                '<div class="flight-content ms-3 p-3">' +
+                '<div class="flight-content__airlines">' +
+                '<img src="' + imagesUrl + ticket.airline + '@2x.png" data-bs-toggle="tooltip" data-bs-placement="top" title="' + (airlines[ticket.airline]?.name ?? "Неизвестно") + '" class="custom-img me-2 rounded-pill">' +
+                '</div>' +
+                '<div class="flight-content__description d-flex mt-3 align-items-center">' +
+                '<div class="flight-content__origin lh-base">' +
+                '<p class="fs-3">' + new Intl.DateTimeFormat(locale, fetchTimeFormatOptions(cities[ticket.origin].timezone)).format(departDate) + '</p>' +
+                '<p class="fs-6 text-secondary">' + origin + '</p>' +
+                '<p class="fs-6 text-secondary">' + new Intl.DateTimeFormat(locale, fetchDateFormatOptions(cities[ticket.origin].timezone)).format(departDate) + '</p>' +
+                '</div>' +
+                '<div class="flight-content__duration mx-5 text-center">' +
+                '<span>В пути: ' + (days ? days + 'д ' : "") + (hours ? hours + 'ч ' : "") + (minutes ? minutes + 'м' : "") + '</span>' +
+                '<p>(пересадок: ' + ticket.transfers + ')</p>' +
+                '</div>' +
+                '<div class="flight-content__destination text-end lh-base">' +
+                '<p class="fs-3">' + new Intl.DateTimeFormat(locale, fetchTimeFormatOptions(cities[ticket.destination].timezone)).format(departDateFinish) + '</p>' +
+                '<p class="fs-6 text-secondary">' + destination + '</p>' +
+                '<p class="fs-6 text-secondary">' + new Intl.DateTimeFormat(locale, fetchDateFormatOptions(cities[ticket.destination].timezone)).format(departDateFinish) + '</p>' +
+                '</div>' +
+                '</div>' +
+                (returnDate ? returnContainer : "")
                 '</div>' +
                 '</div>' +
                 '</div>';
