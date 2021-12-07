0e2c4641b4a908537ee16388d975621f773c84bf
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2021-12-07 22:51:09 +0300

    refactor: clean up code

--- clone-aviasales/Domain/Model/FindAirlinesParams.cs
+++ clone-aviasales/Domain/Model/FindAirlinesParams.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace clone_aviasales.Domain.Model
+﻿namespace clone_aviasales.Domain.Model
 {
     public class FindAirlinesParams
     {
