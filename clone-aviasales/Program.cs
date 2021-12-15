115bec978ab95f9c53c201005b25a43f4fa695ae
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2021-12-15 09:23:15 +0300

    refactor: Make the Program static

--- clone-aviasales/Program.cs
+++ clone-aviasales/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace clone_aviasales
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
